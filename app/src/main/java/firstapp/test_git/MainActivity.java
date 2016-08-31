95f06c76438af43f345bac73582dac7a715ec8a1
esraaHosny <esraa_93_hosny@hotmail.com>
2016-08-31 07:09:55 +0300

    secondCommit

--- app/src/main/java/firstapp/test_git/MainActivity.java
+++ app/src/main/java/firstapp/test_git/MainActivity.java
@@ -1,16 +1,14 @@
 package firstapp.test_git;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,15 +16,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+      button = (Button)findViewById(R.id.button);
     }
+
+
 
     private int Sum(){
         int x = 3;
